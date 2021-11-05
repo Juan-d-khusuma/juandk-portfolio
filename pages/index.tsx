5917fded3c14dcac1456894cab97772588722646
Juan D.K <66712975+Juan-d-khusuma@users.noreply.github.com>
2021-11-05 12:39:02 +0700

    🌙 Dark mode is now loaded and cached to localstorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,38 @@
 /**
- * @author [JuanDKhusuma] {@link https://github.com/Juan-d-khusuma}
+ * @author [JuanDKhusuma](https://github.com/Juan-d-khusuma)
  * @license [MIT]
+ * Feel free to use this as an inspiration for your own projects.
+ * Credit:
+ * [Cody Bennet], for his amazing website and the inspiration for this project.
+ * Also his code on GitHub as a reference for this project
  */
 
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Button from '@components/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DecoderText from '@components/DecoderText';
 import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
 
 const Home: NextPage = (): JSX.Element => {
-    const [darkMode, setDarkmode] = useState<boolean>(false);
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+
+    // Get dark mode preference from localStorage
+    useEffect(() => {
+        const darkMode = localStorage.getItem('darkMode');
+        if (darkMode === 'true') {
+            setDarkMode(true);
+        } else {
+            setDarkMode(false);
+        }
+    }, []);
+
+    // Save dark mode preference to localStorage
+    const handleDarkMode = (): void => {
+        localStorage.setItem('darkMode', String(!darkMode));
+        setDarkMode(!darkMode);
+    };
+
     return (
         <>
             <Head>
@@ -29,10 +50,7 @@ const Home: NextPage = (): JSX.Element => {
                     <DecoderText text="Click Me" />
                 </Button>
 
-                <Button
-                    onClick={() => setDarkmode(!darkMode)}
-                    className="mt-5 ml-3"
-                >
+                <Button onClick={handleDarkMode} className="mt-5 ml-3">
                     {darkMode ? <SunIcon /> : <MoonIcon />}
                 </Button>
             </main>
